68fcd92d8a90d2e9ef2a90835bc48cbf6f5bf212
Viktor Karamanis <viktor.karamanis@outlook.com>
2026-01-08 15:53:31 +0200

    build timestamp

--- drivers/iio/adc/ti-ads131e08.c
+++ drivers/iio/adc/ti-ads131e08.c
@@ -1051,5 +1051,5 @@ module_spi_driver(ads131e08_driver);
 
 MODULE_AUTHOR("Tomislav Denis <tomislav.denis@avl.com>");
 MODULE_AUTHOR("Viktor Karamanis <viktor.karamanis@outlook.com>");
-MODULE_DESCRIPTION("Driver for ADS131E0x ADC family v2");
+MODULE_DESCRIPTION("Driver for ADS131E0x ADC family build:1767880392");
 MODULE_LICENSE("GPL v2");
